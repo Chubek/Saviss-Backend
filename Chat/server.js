cd338e4a8871aa815fad933449b688b8ea88738f
AsenicLupin <arsenic@lupin.com>
2020-06-06 10:34:53 +0430

    changed chat

--- Chat/server.js
+++ Chat/server.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
+let clients = {};
 
 function startServer() {
   const app = express();
   const server = http.createServer(app);
   const wss = new WebSocket.Server({ server });
-  wss.on("connection", function connection(ws) {
+
+  wss.on("connection", function connection(ws, req) {
+    const userID = parseInt(req.url.substr(1), 10);
+    clients[userID] = wss;
+    console.log(
+      "connected: " + userID + " in " + Object.getOwnPropertyNames(wss)
+    );
     ws.on("message", function incoming(data) {
-      wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(data);
-        }
-      });
+      console.log("received from " + userID + ": " + data);
+      const messageArray = JSON.parse(data);
+      const toUserWebSocket = clients[messageArray[0]];
+      if (toUserWebSocket) {
+        console.log(
+          "sent to " + messageArray[0] + ": " + JSON.stringify(messageArray)
+        );
+        messageArray[0] = userID;
+        toUserWebSocket.send(JSON.stringify(messageArray));
+      }
     });
   });
 
