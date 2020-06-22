f07ef51ec555eda3dabf01e3275cc4c6c07a17d0
Chubak <chubakbidpaa@gmail.com>
2020-06-22 10:24:42 +0430

    fixed typo

--- routes/waitingPool.js
+++ routes/waitingPool.js
@@ -2,7 +2,7 @@ const WaitingPool = require("../Models/WaitingPool");
 const router = require("express").Router();
 const ListenerAuth = require("../Middleware/ListenerAuth")
 
-router.get("/get", ListenerAuth(), async (req, res) => {
+router.get("/get", ListenerAuth, async (req, res) => {
     const pool = await WaitingPool.find({listenerId: '', ended: false});
 
     if (pool.length < 1) {
