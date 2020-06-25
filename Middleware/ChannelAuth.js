9e0354897043c73b5cb77cda780ae2575fba072a
Chubak Bidpaa <chubakbidpaa@gmail.com>
2020-06-25 13:03:27 +0430

    typo

--- Middleware/ChannelAuth.js
+++ Middleware/ChannelAuth.js
@@ -2,7 +2,7 @@ const ably = require('ably').Realtime;
 const ablyRealtime = new ably(process.env.ABLY_API);
 
 function ChannelAuth(req, res, next) {
-    const channelName = req.headers("x-session-id");
+    const channelName = req.header("x-session-id");
 
     req.channel = ablyRealtime.channels.get(channelName);
     req.channelName = channelName;
--- Middleware/DecryptMW.js
+++ Middleware/DecryptMW.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const CryptoJS = require("crypto-js");
 
 function DecryptMW(req, res, next) {
-  const pkEnc = req.headers("x-public-key");
+  const pkEnc = req.header("x-public-key");
 
   req.pk = CryptoJS.AES.decrypt(pkEnc, process.env.PK_DEC_KEY);
 
