cb45379f40629354fa8134fdc499945a98e47dc0
Chubak Bidpaa <chubakbidpaa@gmail.com>
2020-07-03 20:50:11 +0430

    added console log

--- Middleware/UserAuth.js
+++ Middleware/UserAuth.js
@@ -3,13 +3,15 @@ const jwt = require("jsonwebtoken");
 module.exports = function UserAuth(req, res, next) {
     const token = req.header("x-auth-number-token");
 
-    if (!token)
-        return res.status(401).json({ message: "No token, access denied." });
+    if (!token) {
+        console.log("No token, access denied. Token is: " + token)
+        return res.status(401).json({message: "No token, access denied."});
+    }
 
     try {
         req.number = jwt.verify(token, process.env.JWT_SECRET);
         next();
     } catch {
-        res.status(400).json({ message: "Token is not valid" });
+        res.status(400).json({message: "Token is not valid"});
     }
 }
